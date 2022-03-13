58e59435618d3e71b25881271ceee576c5d9579a
7Ji <pugokushin@gmail.com>
2022-03-13 12:03:58 +0800

    Split duration class from main script

--- duration.py
+++ duration.py
@@ -1,41 +1,116 @@
-import datetime
-import pathlib
-import subprocess
 import re
-reg_complete = {
-    'video': re.compile(r'video:(\d+)kB'),
-    'audio': re.compile(r'audio:(\d+)kB')
-}
-reg_running = re.compile(r'size= *(\d+)kB')
-reg_time = re.compile(r' time=([0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{2}) ')
-def get_duration_and_size(media: pathlib.Path, stream_id: int, stream_type: str):
-    try:
-        p = subprocess.Popen(('ffmpeg', '-i', media, '-c', 'copy', '-map', f'0:{stream_id}', '-f', 'null', '-'), stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
-        reader = p.stderr
-        while p.poll() is None:
-            chars = []
-            while True:
-                char = reader.read(1)
-                if char in (b'\r', b''):
-                    break
-                elif char != b'\n':
-                    chars.append(char)
-            if chars:
-                line = ''.join([char.decode('utf-8') for char in chars])
-                m = reg_time.search(line)
-                if m:
-                    t = m[1]
-        m = reg_complete[stream_type].search(line)
-        if m:
-            s = int(m[1])
-        p.wait()
-    except KeyboardInterrupt:
-        p.kill()
-    return datetime.timedelta(
-        hours = int(t[:2]),
-        minutes = int(t[3:5]),
-        seconds = float(t[6:])
-    ), s
+class Duration:
+    reg = re.compile()
+    def __init__(self, time):
+        self.update(time)
 
-print(get_duration_and_size('E:\\Python\\220302_SizeCutter\\testFile.mkv', 0, 'video'))
+    def update(self, time: str | int | float):
+        if isinstance(time, str):
 
+            self.time = int(time[:2]) * 3600 + int(time[3:5]) * 60 + float(time[6:])
+        elif isinstance(time, int | float):
+            self.time = time
+        else:
+            raise ValueError(f'Can not initialize a Duration object with {type(time)}')
+
+    def __str__(self):
+        time = int(self.time)
+        hours, remainder = divmod(time, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+
+    def __int__(self):
+        return int(self.time)
+
+    def __float__(self):
+        return float(self.time)
+
+    def __round__(self, ndigits = None):
+        return Duration(round(self.time, ndigits))
+
+    
+    @staticmethod
+    def __complain_comparasion(action, other):
+        raise ValueError(f'Can not {action} Duration with {type(other)}')
+
+    def __add__(self, other:  int | float | str):
+        if isinstance(other, Duration):
+            return Duration(self.time + other.time)
+        elif isinstance(other, int | float):
+            return Duration(self.time + other)
+        elif isinstance(other, str):
+            return Duration(self.time + Duration(other).time)
+        Duration.__complain_comparasion('add', other)
+
+    def __sub__(self, other):
+        if isinstance(other, Duration):
+            return Duration(self.time - other.time)
+        elif isinstance(other, int | float):
+            return Duration(self.time - other)
+        elif isinstance(other, str):
+            return Duration(self.time - Duration(other).time)
+        Duration.__complain_comparasion('substract', other)
+
+    def __mul__(self, other: int | float):
+        if isinstance(other, int | float):
+            return Duration(self.time * other)
+        Duration.__complain_comparasion('multiply', other)
+
+    def __truediv__(self, other: int | float):
+        if isinstance(other, int | float):
+            return Duration(self.time / other)
+        Duration.__complain_comparasion('divide', other)
+
+    def __floordiv__(self, other: int | float):
+        if isinstance(other, int | float):
+            return Duration(self.time // other)
+        Duration.__complain_comparasion('divide', other)
+
+    def __divmod__(self, other: int | float):
+        if isinstance(other, int | float):
+            time, remainder = divmod(self.time, other)
+            return Duration(time), Duration(remainder)
+        Duration.__complain_comparasion('divide', other)
+
+    def __lt__(self, other):
+        if isinstance(other, Duration):
+            if self.time < other.time:
+                return True
+            else:
+                return False
+        elif isinstance(other, int | float):
+            if self.time < other:
+                return True
+            else:
+                return False
+        elif isinstance(other, str):
+            if self.time < Duration(other).time:
+                return True
+            else:
+                return False
+        Duration.__complain_comparasion('compare', other)
+
+    def __le__(self, other):
+        if isinstance(other, Duration):
+            if self.time <= other.time:
+                return True
+            else:
+                return False
+        elif isinstance(other, int | float):
+            if self.time <= other:
+                return True
+            else:
+                return False
+        elif isinstance(other, str):
+            if self.time <= Duration(other).time:
+                return True
+            else:
+                return False
+        Duration.__complain_comparasion('compare', other)
+
+    
+    # object.__le__(self, other)
+    # object.__eq__(self, other)¶
+    # object.__ne__(self, other)
+    # object.__gt__(self, other)
+    # object.__ge__(self, other)
--- videoArchiver.py
+++ videoArchiver.py
@@ -14,7 +14,8 @@ import time
 import pickle
 import math
 import logging
-
+# The following modules are written
+import duration
 
 time_zero = datetime.timedelta()
 time_second = datetime.timedelta(seconds=1)
@@ -30,13 +31,6 @@ class CleanPrinter:
     bars = 0
     lock_bar = threading.Lock()
     width = shutil.get_terminal_size()[0]
-
-    # @staticmethod
-    # def check_terminal():
-    #     width = shutil.get_terminal_size()[0]
-    #     if width == CleanPrinter.width:
-    #         return 
-    #     CleanPrinter.width = width
 
     @staticmethod
     def clear_line():
@@ -65,79 +59,6 @@ class CleanPrinter:
                 logging.debug(content)
             case _:
                 pass
-        
-
-class Duration:
-    def __init__(self, time):
-        self.update(time)
-
-    def update(self, time: str | int | float):
-        if isinstance(time, str):
-            self.time = int(time[:2]) * 3600 + int(time[3:5]) * 60 + float(time[6:])
-        elif isinstance(time, int | float):
-            self.time = time
-        else:
-            raise ValueError(f'Can not initialize a Duration object with {type(time)}')
-
-    def __str__(self):
-        time = int(self.time)
-        hours, remainder = divmod(time, 3600)
-        minutes, seconds = divmod(remainder, 60)
-        return f'{hours:02d}:{minutes:02d}:{seconds:02d}'
-
-    def __int__(self):
-        return int(self.time)
-
-    def __float__(self):
-        return float(self.time)
-
-    def __round__(self, ndigits = None):
-        return Duration(round(self.time, ndigits))
-
-    def __add__(self, other:  int | float | str):
-        if isinstance(other, Duration):
-            return Duration(self.time + other.time)
-        elif isinstance(other, int | float):
-            return Duration(self.time + other)
-        elif isinstance(other, str):
-            return Duration(self.time + Duration(other).time)
-        else:
-            raise ValueError(f'Can not add Duration with {type(other)}')
-
-    def __sub__(self, other):
-        if isinstance(other, Duration):
-            return Duration(self.time - other.time)
-        elif isinstance(other, int | float):
-            return Duration(self.time - other)
-        elif isinstance(other, str):
-            return Duration(self.time - Duration(other).time)
-        else:
-            raise ValueError(f'Can not subtract Duration with {type(other)}')
-
-    def __mul__(self, other: int | float):
-        if isinstance(other, int | float):
-            return Duration(self.time * other)
-        else:
-            raise ValueError(f'Can not multiply Duration with {type(other)}')
-
-    def __truediv__(self, other: int | float):
-        if isinstance(other, int | float):
-            return Duration(self.time / other)
-        else:
-            raise ValueError(f'Can not divide Duration with {type(other)}')
-
-    def __floordiv__(self, other: int | float):
-        if isinstance(other, int | float):
-            return Duration(self.time // other)
-        else:
-            raise ValueError(f'Can not divide Duration with {type(other)}')
-
-    def __divmod__(self, other: int | float):
-        if isinstance(other, int | float):
-            time, remainder = divmod(self.time, other)
-            return Duration(time), Duration(remainder)
-        else:
-            raise ValueError(f'Can not divide Duration with {type(other)}')
 
 
 def clamp(n, minimum, maximum):
